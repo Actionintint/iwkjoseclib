566bb15d90b37dd6abe44aeaec974cb8c8538c4d
Actionintint <Actionintint@outlook.jp>
2018-06-09 14:55:05 +0900

    C(n, r)

--- Func.cs
+++ Func.cs
@@ -39,6 +39,36 @@
         return res;
     }
 
+    long[] fac;
+    long[] finv;
+    long[] inv;
+    const int mod = (int)1e9 + 7;
+    const int nMax = 5*(int)1e5;
+
+    void InitC()
+    {
+        fac = new long[nMax];
+        finv = new long[nMax];
+        inv = new long[nMax];
+
+        fac[0] = fac[1] = 1;
+        finv[0] = finv[1] = 1;
+        inv[1] = 1;
+        for (int i = 2; i < nMax; i++)
+        {
+            fac[i] = fac[i - 1] * i % mod;
+            inv[i] = mod - inv[mod % i] * (mod / i) % mod;
+            finv[i] = finv[i - 1] * inv[i] % mod;
+        }
+    }
+
+    long C(int n, int r)
+    {
+        if (n < r) return 0;
+        if (n < 0 || r < 0) return 0;
+        return fac[n] * (finv[r] * finv[n - r] % mod) % mod;
+    }
+    
     int BitCount(int n)
     {
         n = (n & 0x55555555) + (n >> 1 & 0x55555555);
