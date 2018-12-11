fe93a721cb330e6f597ed451ce47d33886842909
Actionintint <Actionintint@outlook.jp>
2018-12-12 01:27:45 +0900

    フル機能Xorshiftの追加

--- Xorshift.cs
+++ Xorshift.cs
@@ -27,3 +27,67 @@ class Xorshift
 
     public int Next(int maxValue) => (int)(Next() % maxValue);
 }
+
+public class Xorshift
+{
+    uint x = 123456789;
+    uint y = 362436069;
+    uint z = 521288629;
+    uint w = 88675123;
+
+    public Xorshift() : this(Environment.TickCount)
+    {
+
+    }
+
+    public Xorshift(int seed)
+    {
+        var t = (uint)seed;
+        x ^= t;
+        y ^= Shift(t, 17);
+        z ^= Shift(t, 31);
+        w ^= Shift(t, 18);
+    }
+
+    uint Shift(uint u, int n) => u << n | u >> 32 - n;
+
+    public int Next()
+    {
+        var t = x ^ x << 11;
+        x = y; y = z; z = w;
+        t = w = w ^ w >> 19 ^ t ^ t >> 8;
+        t &= int.MaxValue;
+        return t == int.MaxValue ? int.MaxValue - 1 : (int)t;
+    }
+
+    public int Next(int minValue, int maxValue) => Next(maxValue - minValue) + minValue;
+
+    uint Xorshift128()
+    {
+        var t = x ^ x << 11;
+        x = y; y = z; z = w;
+        return w = w ^ w >> 19 ^ t ^ t >> 8;
+    }
+
+    public int Next(int maxValue) => (int)(Xorshift128() % maxValue);
+
+    public double NextDouble() => Next() * (1.0 / int.MaxValue);
+
+    public void NextBytes(byte[] buffer)
+    {
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            buffer[i] = (byte)Next();
+        }
+    }
+
+#if NETCOREAPP2_1 || NETCOREAPP2_2
+    public void NextBytes(Span<byte> buffer)
+    {
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            buffer[i] = (byte)Next();
+        }
+    }
+#endif
+}
