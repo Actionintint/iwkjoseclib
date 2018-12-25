f60b83789234dd17bf21e2259ad85239a3f70d5a
Actionintint <Actionintint@outlook.jp>
2018-12-26 01:49:43 +0900

    Xorshiftのディレクトリ構造変更してファイル分割

---
+++ PRNG/Xorshift/MinimumXorshift.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Xorshift
+{
+    uint x = 123456789;
+    uint y = 362436069;
+    uint z = 521288629;
+    uint w = 88675123;
+
+    public Xorshift()
+    {
+        var t = (uint)Environment.TickCount;
+        x ^= t;
+        y ^= Shift(t, 17);
+        z ^= Shift(t, 31);
+        w ^= Shift(t, 18);
+    }
+
+    uint Shift(uint u, int n) => u << n | u >> 32 - n;
+
+    uint Next()
+    {
+        var t = x ^ x << 11;
+        x = y; y = z; z = w;
+        return w = w ^ w >> 19 ^ t ^ t >> 8;
+    }
+
+    public int Next(int maxValue) => (int)(Next() % maxValue);
+}
