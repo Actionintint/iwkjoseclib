02d8162c4d04c3ae259b13c4675356582b98becf
Actionintint <Actionintint@outlook.jp>
2018-12-25 18:49:54 +0900

    Pow(double, double)削除

--- Func.cs
+++ Func.cs
@@ -38,18 +38,6 @@
         }
         return res;
     }
-    
-    long Pow(double x, double y)
-    {
-        var res = 1.0;
-        while (y != 0)
-        {
-            if ((y & 1) != 0) res = res * x;
-            x = x * x;
-            y >>= 1;
-        }
-        return res;
-    }
 
     int[,] MatrixMul(int[,] x, int[,] y)
     {
