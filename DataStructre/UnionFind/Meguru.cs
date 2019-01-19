ed3a9a2a3dad06ff811a426e0115ffbc473939e3
Actionintint <Actionintint@outlook.jp>
2019-01-20 02:48:32 +0900

    Meguru再々追加　実装変更
    #25

---
+++ DataStructre/UnionFind/Meguru.cs
@@ -0,0 +1,40 @@
+﻿public class UnionFind
+{
+    int[] parent;
+    public int Count { get; private set; }
+
+    public UnionFind(int size)
+    {
+        var b = new int[size];
+        for (int i = 0; i < b.Length; i++)
+        {
+            b[i] = -1;
+        }
+        parent = b;
+        Count = size;
+    }
+
+    int Root(int x) => parent[x] < 0 ? x : parent[x] = Root(parent[x]);
+
+    public bool Some(int x, int y) => Root(x) == Root(y);
+
+    public void Unite(int x, int y)
+    {
+        x = Root(x);
+        y = Root(y);
+        if (x == y) return;
+        Count--;
+        if (parent[x] < parent[y])
+        {
+            parent[y] += parent[x];
+            parent[x] = y;
+        }
+        else
+        {
+            parent[x] += parent[y];
+            parent[y] = x;
+        }
+    }
+
+    public int Size(int x) => -parent[Root(x)];
+}
