112f51312f9627175dc6f5254b3f660a36e3d6ae
Actionintint <Actionintint@outlook.jp>
2019-01-20 19:24:12 +0900

    Map変更
    #8

--- DataStructre/Map/Map.cs
+++ DataStructre/Map/Map.cs
@@ -8,18 +8,12 @@ public class Map<TKey, TValue> : Dictionary<TKey, TValue>
     public Map(int capacity, IEqualityComparer<TKey> comparer) : base(capacity, comparer) { }
     public Map(IDictionary<TKey, TValue> dictionary) : base(dictionary) { }
     public Map(IDictionary<TKey, TValue> dictionary, IEqualityComparer<TKey> comparer) : base(dictionary, comparer) { }
-    public Map(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    public Map(IEnumerable<KeyValuePair<TKey, TValue>> collection) : base(collection) { }
+    public Map(IEnumerable<KeyValuePair<TKey, TValue>> collection, IEqualityComparer<TKey> comparer) : base(collection, comparer) { }
 
     public new TValue this[TKey key]
     {
-        get
-        {
-            TValue value;
-            return TryGetValue(key, out value) ? value : default(TValue);
-        }
-        set
-        {
-            base[key] = value;
-        }
+        get => TryGetValue(key, out var value) ? value : default;
+        set => base[key] = value;
     }
 }
