d73735e93c52316054cd833c377fb79e46cc9af1
Actionintint <Actionintint@outlook.jp>
2019-01-22 13:06:29 +0900

    Create Util.cs

---
+++ Utility/Util.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+public static class Util
+{
+    public static unsafe void Bin<T>(in T value) where T : unmanaged
+    {
+        var s = stackalloc byte[sizeof(T)];
+        *(T*)s = value;
+        for (int i = sizeof(T) - 1; i >= 0; i--)
+        {
+            Write(Convert.ToString(s[i], 2).PadLeft(8, '0'));
+        }
+        WriteLine();
+    }
+
+    public static unsafe void Hex<T>(in T value) where T : unmanaged
+    {
+        var s = stackalloc byte[sizeof(T)];
+        *(T*)s = value;
+        for (int i = sizeof(T) - 1; i >= 0; i--)
+        {
+            Write(s[i].ToString("X").PadLeft(2, '0'));
+        }
+        WriteLine();
+    }
+}
