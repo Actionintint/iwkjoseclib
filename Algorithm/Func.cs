d4100e245a070eb0913236bef4a524693da81ace
Actionintint <Actionintint@outlook.jp>
2019-05-19 02:10:39 +0900

    Update Func.cs

--- Algorithm/Func.cs
+++ Algorithm/Func.cs
@@ -1,4 +1,6 @@
-﻿class Func
+﻿using System;
+
+class Func
 {
     public static int GCD(int a, int b)
     {
@@ -14,8 +16,7 @@
     public static bool IsPrime(int n)
     {
         if (n < 2) return false;
-        if (n == 2) return true;
-        if ((n & 1) == 0) return false;
+        if (n % 2 == 0) return n == 2;
         var r = (int)Math.Sqrt(n);
         for (int i = 3; i <= r; i += 2) if (n % i == 0) return false;
         return true;
