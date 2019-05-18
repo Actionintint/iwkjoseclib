d4f171ad88c72397c9d8ace56d656a09cd7d5943
Actionintint <Actionintint@outlook.jp>
2019-05-18 23:37:43 +0900

    Update Minimum.cs

--- PRNG/Xorshift/Minimum.cs
+++ PRNG/Xorshift/Minimum.cs
@@ -18,12 +18,12 @@ public class Xorshift
         uint Shift(uint u, int n) => u << n | u >> 32 - n;
     }
 
-    uint Next()
+    uint Xorshift128()
     {
         var t = x ^ x << 11;
         x = y; y = z; z = w;
         return w = w ^ w >> 19 ^ t ^ t >> 8;
     }
 
-    public int Next(int maxValue) => (int)(Next() % (uint)maxValue);
+    public int Next(int maxValue) => (int)(Xorshift128() % (uint)maxValue);
 }
