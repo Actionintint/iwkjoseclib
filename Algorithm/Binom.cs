47c16cd29bb91b3f609700d01e4ee5bcde750541
Actionintint <Actionintint@outlook.jp>
2019-05-26 15:38:16 +0900

    二項係数分離

---
+++ Algorithm/Binom.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class BinomialCoefficient
+{
+    long[] fac;
+    long[] finv;
+    long[] inv;
+    int mod;
+
+    public BinomialCoefficient(int N, int p = (int)1e9 + 7)// O(N)
+    {
+        N++;
+
+        fac = new long[N];
+        finv = new long[N];
+        inv = new long[N];
+
+        fac[0] = fac[1] = 1;
+        finv[0] = finv[1] = 1;
+        inv[1] = 1;
+        for (int i = 2; i < N; i++)
+        {
+            fac[i] = fac[i - 1] * i % p;
+            inv[i] = p - inv[p % i] * (p / i) % p;
+            finv[i] = finv[i - 1] * inv[i] % p;
+        }
+
+        mod = p;
+    }
+
+    public long Binom(int n, int k)// O(1)
+    {
+        if (n < k) return 0;
+        if (n < 0 || k < 0) return 0;
+        return fac[n] * finv[k] % mod * finv[n - k] % mod;
+    }
+}
--- Algorithm/Func.cs
+++ Algorithm/Func.cs
@@ -55,36 +55,6 @@ class Func
         return m;
     }
 
-    long[] fac;
-    long[] finv;
-    long[] inv;
-    const int mod = (int)1e9 + 7;
-    const int nMax = 5 * (int)1e5;
-
-    void InitC()
-    {
-        fac = new long[nMax];
-        finv = new long[nMax];
-        inv = new long[nMax];
-
-        fac[0] = fac[1] = 1;
-        finv[0] = finv[1] = 1;
-        inv[1] = 1;
-        for (int i = 2; i < nMax; i++)
-        {
-            fac[i] = fac[i - 1] * i % mod;
-            inv[i] = mod - inv[mod % i] * (mod / i) % mod;
-            finv[i] = finv[i - 1] * inv[i] % mod;
-        }
-    }
-
-    long C(int n, int r)
-    {
-        if (n < r) return 0;
-        if (n < 0 || r < 0) return 0;
-        return fac[n] * (finv[r] * finv[n - r] % mod) % mod;
-    }
-
     double Differential(Func<double, double> f, double x)
     {
         return (f(x + 1.0 / 1000000) - f(x - 1.0 / 1000000)) / (1.0 / 500000);
