4ec4a16ecf51a25f3e8750b5fa351cb164454bd1
Actionintint <Actionintint@outlook.jp>
2019-06-04 01:50:57 +0900

    Update Binom.cs

--- Algorithm/Binom.cs
+++ Algorithm/Binom.cs
@@ -4,7 +4,6 @@ class ModBinom
 {
     long[] fac;
     long[] finv;
-    long[] inv;
     int mod;
 
     public ModBinom(int N, int p = (int)1e9 + 7)//N < p  O(N)
@@ -12,12 +11,13 @@ class ModBinom
         N++;
 
         fac = new long[N];
+        long[] inv = new long[N];
         finv = new long[N];
-        inv = new long[N];
 
         fac[0] = fac[1] = 1;
-        finv[0] = finv[1] = 1;
         inv[1] = 1;
+        finv[0] = finv[1] = 1;
+
         for (int i = 2; i < N; i++)
         {
             fac[i] = fac[i - 1] * i % p;
